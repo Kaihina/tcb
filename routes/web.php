ba6c32c2376adbbd77c9cc87726ce246df838b7e
Kaihina <hinata.k.2000@icloud.com>
2023-04-05 01:34:03 +0900

    second commit

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +14,4 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{post}', [PostController::class ,'show']);
