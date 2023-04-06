0f6024df8f6f44d69bf03f65497767756bdc4e5a
Kaihina <hinata.k.2000@icloud.com>
2023-04-06 19:22:35 +0900

    sixth commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,25 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     
     protected $fillable = [
-    'title',
-    'body',
-    ];
+        'title',
+        'body',
+        ];
     
     public function getPaginateByLimit(int $limit_count = 5)
     {
         return $this->orderby('updated_at', 'DESC')->Paginate($limit_count);
-    }
-    
-    public function store(Request $request, Post $post)
-    {
-    $input = $request['post'];
-    $post->fill($input)->save();
-    return redirect('/posts/' . $post->id);
     }
 }
